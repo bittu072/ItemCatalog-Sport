d7b38c93607ae9af55d577a9c26375e870ed2424
bittu072 <bittu.patel072@gmail.com>
2017-01-25 23:23:55 -0800

    added comments

--- project.py
+++ project.py
@@ -671,5 +671,7 @@ def teamJSON(team_id):
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
+    # always turn off debugging
+    # when the application moves to production environment.
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
